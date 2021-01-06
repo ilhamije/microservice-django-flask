3b4dfc9ee693bef26562cef1f50b3479fa30b1b1
ilhamije <ilhamije@gmail.com>
2021-01-06 10:50:59 +0700

    [add] api test for create product

--- admin/products/tests.py
+++ admin/products/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Product
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 
 
 class ProductTestCase(TestCase):
@@ -12,5 +15,27 @@ class ProductTestCase(TestCase):
         self.assertEquals(title.image, 'image test')
 
     def test_get_one_product(self):
-        title = Product.objects.get(pk=self.test_product_id)
-        self.assertEquals(title.image, 'image test')
+        product = Product.objects.get(pk=self.test_product_id)
+        self.assertEquals(product.image, 'image test')
+
+    # def test_update_one_product(self):
+    #     product = Product.objects.get(pk=self.test_product_id)
+    #     updated_product = Product.objects.update(image='image updated')
+    #     self.assertEquals(updated_product.image, 'image updated')
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.productlist_data = {
+            'title':'product api test',
+            'image':'imagine the future'
+            }
+        self.response = self.client.post(
+            reverse('products-list-create'),
+            self.productlist_data,
+            format='json'
+        )
+
+    def test_api_create_product(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- admin/products/urls.py
+++ admin/products/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('products', ProductViewSet.as_view({
         'get': 'list',
         'post': 'create'
-    })),
+    }), name='products-list-create'),
     path('products/<str:pk>', ProductViewSet.as_view({
         'get':'retrieve',
         'put':'update',
         'delete':'destroy',
-    }))
+    }), name='product-get-put-delete')
 ]
