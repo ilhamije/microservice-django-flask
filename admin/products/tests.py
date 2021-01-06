3a85ee73a7513f9096adbb9d90d3db08f2bae1dc
ilhamije <ilhamije@gmail.com>
2021-01-06 23:23:13 +0700

    [update] product test, create single product

--- admin/products/tests.py
+++ admin/products/tests.py
@@ -1,9 +1,11 @@
-from django.test import TestCase
-from .models import Product
-from .serializers import ProductSerializer
+import json
 from rest_framework.test import APIClient
 from rest_framework import status
+from django.test import TestCase
 from django.urls import reverse
+
+from .models import Product
+from .serializers import ProductSerializer
 
 
 # initialize the APIClient App
@@ -11,6 +13,8 @@ client = APIClient()
 
 
 class GetAllProductsTest(TestCase):
+    """ Test module to GET all product API """
+
     def setUp(self):
         Product.objects.create(title="snack 1", image="image item 1")
         Product.objects.create(title="yugurt 2", image="image item 2")
@@ -25,6 +29,7 @@ class GetAllProductsTest(TestCase):
 
 
 class GetSingleProductTest(TestCase):
+    """ Test module to GET single product API """
     def setUp(self):
         self.snack = Product.objects.create(title="snack", image="image item 1")
         self.yugurt = Product.objects.create(title="yugurt", image="image item 2")
@@ -40,3 +45,33 @@ class GetSingleProductTest(TestCase):
     def test_get_invalid_single_product(self):
         response = client.get(reverse('products-detail', kwargs={'pk':91}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class CreateNewProductTest(TestCase):
+    """ Test module for inserting a new product """
+
+    def setUp(self):
+        self.valid_payload = {
+            'title': 'Karbol',
+            'image': 'Paid karbol'
+        }
+        self.invalid_payload = {
+            'title': '',
+            'image': 'Paid karbol'
+        }
+
+    def test_create_valid_product(self):
+        response = client.post(
+            reverse('products-list-create'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_product(self):
+        response = client.post(
+            reverse('products-list-create'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
